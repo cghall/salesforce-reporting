41a9a9aba4acc1588fd5ed4f07ec77e3da3d7416
Chris Hall <christopherhall17@gmail.com>
2016-01-14 20:28:32 +0000

    Add ignore to test method not implemented

--- test/test_report_parser.py
+++ test/test_report_parser.py
@@ -15,9 +15,17 @@ class ReportParserTest(unittest.TestCase):
         report = ReportParser(self.build_mock_report('test/test_data/simple_tabular.json'))
 
         records = report.records()
-
         self.assertEquals(len(records), 34)
         self.assertEquals(records[0][0], 'P-0167')
+
+    def test_get_record_dict_simple_tabular(self):
+        report = ReportParser(self.build_mock_report('test/test_data/simple_tabular.json'))
+
+        records = report.records_dict()
+        field_value = records[0]["Programme: Programme Number"]
+
+        self.assertEquals(len(records), 34)
+        self.assertIsNotNone(field_value)
 
     def test_get_records_simple_summary(self):
         report = ReportParser(self.build_mock_report('test/test_data/simple_summary.json'))
@@ -25,6 +33,15 @@ class ReportParserTest(unittest.TestCase):
         records = report.records()
 
         self.assertEquals(len(records), 34)
+
+    def test_get_record_dict_simple_summary(self):
+        report = ReportParser(self.build_mock_report('test/test_data/simple_summary.json'))
+
+        records = report.records_dict()
+        field_value = records[0]["Programme: Programme Number"]
+
+        self.assertEquals(len(records), 34)
+        self.assertIsNotNone(field_value)
 
     def test_get_records_basic_matrix(self):
         report = ReportParser(self.build_mock_report('test/test_data/basic_matrix.json'))
@@ -37,6 +54,11 @@ class ReportParserTest(unittest.TestCase):
         report = ReportParser(self.build_mock_report('test/test_data/multiple_matrix.json'))
 
         self.assertRaises(ValueError, report.records)
+
+    def test_get_records_dict_without_details(self):
+        report = ReportParser(self.build_mock_report('test/test_data/multiple_matrix.json'))
+
+        self.assertRaises(ValueError, report.records_dict)
 
     def test_get_grand_total_on_summary_report(self):
         report = ReportParser(self.build_mock_report('test/test_data/simple_summary.json'))
