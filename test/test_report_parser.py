4950ca6eaf28a5dc896a4cb274ae388c4202a944
Andrew Walker <awalkerdev@gmail.com>
2016-01-19 21:33:26 +0000

    Tests pass on Windows after fixing paths

--- test/test_report_parser.py
+++ test/test_report_parser.py
@@ -1,25 +1,18 @@
-import json
-import unittest
-
 from salesforce_reporting import ReportParser
+from test.common import ParserTest
 
 
-class ReportParserTest(unittest.TestCase):
-
-    def build_mock_report(self, fp):
-        with open(fp) as json_file:
-             report_data = json.load(json_file)
-        return report_data
+class ReportParserTest(ParserTest):
 
     def test_get_records_simple_tabular(self):
-        report = ReportParser(self.build_mock_report('test/test_data/tabular_basic.json'))
+        report = ReportParser(self.build_mock_report('tabular_basic'))
 
         records = report.records()
         self.assertEquals(len(records), 20)
         self.assertEquals(records[1][2], 'James')
 
     def test_get_record_dict_simple_tabular(self):
-        report = ReportParser(self.build_mock_report('test/test_data/tabular_basic.json'))
+        report = ReportParser(self.build_mock_report('tabular_basic'))
 
         records = report.records_dict()
         field_value = records[0]["Mailing Street"]
@@ -28,7 +21,7 @@ class ReportParserTest(unittest.TestCase):
         self.assertIsNotNone(field_value)
 
     def test_get_records_simple_summary(self):
-        report = ReportParser(self.build_mock_report('test/test_data/summary_basic_single_group.json'))
+        report = ReportParser(self.build_mock_report('summary_basic_single_group'))
 
         records = report.records()
 
@@ -36,7 +29,7 @@ class ReportParserTest(unittest.TestCase):
         self.assertEquals(records[0][0], 'Chris Hall')
 
     def test_get_record_dict_simple_summary(self):
-        report = ReportParser(self.build_mock_report('test/test_data/summary_basic_single_group.json'))
+        report = ReportParser(self.build_mock_report('summary_basic_single_group'))
 
         records = report.records_dict()
         field_value = records[0]["Created Date"]
@@ -45,31 +38,31 @@ class ReportParserTest(unittest.TestCase):
         self.assertIsNotNone(field_value)
 
     def test_get_records_basic_matrix(self):
-        report = ReportParser(self.build_mock_report('test/test_data/matrix_basic.json'))
+        report = ReportParser(self.build_mock_report('matrix_basic'))
 
         records = report.records()
 
         self.assertEquals(len(records), 18)
 
     def test_get_records_without_details(self):
-        report = ReportParser(self.build_mock_report('test/test_data/matrix_complex.json'))
+        report = ReportParser(self.build_mock_report('matrix_complex'))
 
         self.assertRaises(ValueError, report.records)
 
     def test_get_records_dict_without_details(self):
-        report = ReportParser(self.build_mock_report('test/test_data/matrix_complex.json'))
+        report = ReportParser(self.build_mock_report('matrix_complex'))
 
         self.assertRaises(ValueError, report.records_dict)
 
     def test_get_grand_total_on_summary_report(self):
-        report = ReportParser(self.build_mock_report('test/test_data/summary_basic_single_group.json'))
+        report = ReportParser(self.build_mock_report('summary_basic_single_group'))
 
         grand_total = report.get_grand_total()
 
         self.assertEquals(grand_total, 9469000000)
 
     def test_get_grand_total_on_matrix_report(self):
-        report = ReportParser(self.build_mock_report('test/test_data/matrix_basic.json'))
+        report = ReportParser(self.build_mock_report('matrix_basic'))
 
         grand_total = report.get_grand_total()
 
