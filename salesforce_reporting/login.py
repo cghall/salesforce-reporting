b24a34908656151ed219c41c7014dbafa8a68be2
Chris Hall <christopherhall17@gmail.com>
2016-04-02 18:05:25 +0100

    Allow Connection to use Sandbox instance and custom API version by implementing a _get_login_url method, add associated tests

--- salesforce_reporting/login.py
+++ salesforce_reporting/login.py
@@ -11,21 +11,34 @@ except ImportError:
 
 class Connection:
     """
-    Create a valid Salesforce connection for accessing the Salesforce Analytics API using
-    the Password authentication method.
+    A Salesforce connection for accessing the Salesforce Analytics API using
+    the Password/Token authentication. This object is then used as the central
+    object for passing report requests into Salesforce.
+
+    By default the object assumes you are connection to a Production instance
+    and using API v29.0 but both of these can be overridden to allow access to Sandbox
+    instances and/or use a different API version.
 
     Parameters
     ----------
-    username: the Salesforce username used for authentication
-    password: the Salesforce password used for authentication
-    security_token: the Salesforce security token used for authentication
+    username: string
+        the Salesforce username used for authentication
+    password: string
+        the Salesforce password used for authentication
+    security_token: string
+        the Salesforce security token used for authentication (normally tied to password)
+    sandbox: boolean, default False
+        whether or not the Salesforce instance connected to is a Sandbox
+    api_version: string
 
     """
 
-    def __init__(self, username=None, password=None, security_token=None):
+    def __init__(self, username=None, password=None, security_token=None, sandbox=False, api_version='v29.0'):
         self.username = username
         self.password = password
         self.security_token = security_token
+        self.sandbox = sandbox
+        self.api_version = api_version
         self.login_details = self.login(self.username, self.password, self.security_token)
         self.token = self.login_details['oauth']
         self.instance = self.login_details['instance']
@@ -33,22 +46,29 @@ class Connection:
         self.base_url = 'https://{}/services/data/v31.0/analytics'.format(self.instance)
 
     @staticmethod
-    def getUniqueElementValueFromXmlString(xml_string, element_name):
-        xml_string_as_dom = xml.dom.minidom.parseString(xml_string)
-        elements_by_name = xml_string_as_dom.getElementsByTagName(element_name)
+    def element_from_xml_string(xml_string, element):
+        xml_as_dom = xml.dom.minidom.parseString(xml_string)
+        elements_by_name = xml_as_dom.getElementsByTagName(element)
         element_value = None
 
         if len(elements_by_name) > 0:
-            element_value = elements_by_name[0].toxml().replace('<' + element_name + '>', '').replace(
-                '</' + element_name + '>', '')
+            element_value = elements_by_name[0].toxml().replace('<' + element + '>', '').replace(
+                '</' + element + '>', '')
 
         return element_value
+
+    @staticmethod
+    def _get_login_url(is_sandbox, api_version):
+        if is_sandbox:
+            return 'https://{}.salesforce.com/services/Soap/u/{}'.format('test', api_version)
+        else:
+            return 'https://{}.salesforce.com/services/Soap/u/{}'.format('login', api_version)
 
     def login(self, username, password, security_token):
         username = escape(username)
         password = escape(password)
 
-        url = 'https://login.salesforce.com/services/Soap/u/v29.0'
+        url = self._get_login_url(self.sandbox, self.api_version)
 
         request_body = """<?xml version="1.0" encoding="utf-8" ?>
         <env:Envelope
@@ -73,13 +93,13 @@ class Connection:
         response = requests.post(url, request_body, headers=request_headers)
 
         if response.status_code != 200:
-            exception_code = self.getUniqueElementValueFromXmlString(response.content, 'sf:exceptionCode')
-            exception_msg = self.getUniqueElementValueFromXmlString(response.content, 'sf:exceptionMessage')
+            exception_code = self.element_from_xml_string(response.content, 'sf:exceptionCode')
+            exception_msg = self.element_from_xml_string(response.content, 'sf:exceptionMessage')
 
             raise AuthenticationFailure(exception_code, exception_msg)
 
-        oauth_token = self.getUniqueElementValueFromXmlString(response.content, 'sessionId')
-        server_url = self.getUniqueElementValueFromXmlString(response.content, 'serverUrl')
+        oauth_token = self.element_from_xml_string(response.content, 'sessionId')
+        server_url = self.element_from_xml_string(response.content, 'serverUrl')
 
         instance = (server_url.replace('http://', '')
                      .replace('https://', '')
--- test/test_login.py
+++ test/test_login.py
@@ -5,7 +5,14 @@ from salesforce_reporting import Connection, AuthenticationFailure
 
 class ConnectionTest(unittest.TestCase):
 
-    @unittest.skip("Skip for normal tests - runtime length")
     def test_incorrect_password_raises_exception(self):
         self.assertRaises(AuthenticationFailure, Connection, username="fake@user.com", password="1234",
                           security_token="5678")
+
+    def test_default_login_url(self):
+        self.assertEquals(Connection._get_login_url(False, 'v29.0'),
+                          'https://login.salesforce.com/services/Soap/u/v29.0')
+
+    def test_sandbox_url_with_different_api(self):
+        self.assertEquals(Connection._get_login_url(True, 'v33.0'),
+                          'https://test.salesforce.com/services/Soap/u/v33.0')
